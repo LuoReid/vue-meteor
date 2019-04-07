a09c54946152487ee8cd1622f6bf39b1d1677505
Luo Reid <luo.reid@gmail.com>
2019-04-07 20:28:38 +0800

    report dashboard

--- client/main.js
+++ client/main.js
@@ -4,8 +4,14 @@ import App from './components/App.vue'
 
 import router from './router'
 import VueMeteorTracker from 'vue-meteor-tracker'
+
+import 'vue-progress-path/dist/vue-progress-path.css'
+import VueProgress from 'vue-progress-path'
+
 Vue.use(VueMeteorTracker)
 
+Vue.use(VueProgress, { defaultShape: 'semicircle' })
+
 Meteor.startup(() => {
-  new Vue({ el: '#app', router,...App, })
+  new Vue({ el: '#app', router, ...App, })
 })
