90a24aa863dd6c993bd1d239019b93d0ad209aad
Luo Reid <luo.reid@gmail.com>
2019-04-07 18:48:13 +0800

    set data

--- client/components/ProductionGenerator.vue
+++ client/components/ProductionGenerator.vue
@@ -1,3 +1,22 @@
 <template>
-    
+  <div class="production-generator">
+    <h1>Measure production</h1>
+    <section class="actions">
+      <button v-on:click="generateMeasure(false)">Generate Measure</button>
+      <button v-on:click="generateMeasure(true)">Generate Error</button>
+    </section>
+  </div>
 </template>
+<script>
+import { Meteor } from "meteor/meteor";
+export default {
+  methods: {
+    generateMeasure(error) {
+      const value = Math.round(Math.random() * 100);
+      const measure = { value, error };
+      Meteor.call("measure.add", measure);
+    }
+  }
+};
+</script>
+
---
+++ client/lib/methods.js
@@ -0,0 +1,10 @@
+import { Meteor } from 'meteor/meteor'
+import { Measures } from './collections'
+
+Meteor.methods({
+  'measure.add'(measure) {
+    Measures.insert({
+      ...measure, date: new Date(),
+    })
+  }
+})
