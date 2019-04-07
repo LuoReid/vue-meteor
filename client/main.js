7934c6005b5b76a0c82e5054e76c8618b4188bd0
Luo Reid <luo.reid@gmail.com>
2019-04-07 18:26:36 +0800

    import meteor connection

--- client/main.js
+++ client/main.js
@@ -3,6 +3,8 @@ import Vue from 'vue'
 import App from './components/App.vue'
 
 import router from './router'
+import VueMeteorTracker from 'vue-meteor-tracker'
+Vue.use(VueMeteorTracker)
 
 Meteor.startup(() => {
   new Vue({ el: '#app', router,...App, })
